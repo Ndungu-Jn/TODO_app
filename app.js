52ce79b2d1189df79a75b8e163d76587ed3604b3
John Ndungu <ndungulink@gmail.com>
2025-04-10 13:46:22 +0300

    Added string literals and some functions.

--- app.js
+++ app.js
@@ -22,14 +22,55 @@ function addTodo() {
   const todoText = todoInput.value.trim();
   if (todoText.length > 0) {
     allTodos.push(todoText);
-    createTodoItem(todoText);
+    updateTodoList();
     console.log(allTodos);
     todoInput.value = "";
   }
 }
+//anytime the value inside the array changes, update the to do list
+function updateTodoList() {
+  todoListUL.innerHTML = "";
+  allTodos.forEach((todo, todoIndex) => {
+    todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
 
-function createTodoItem(todo) {
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoindex;
   const todoLi = document.createElement("li");
-  todoLi.innerText = todo;
-  todoListUL.append(todoLi);
+  todoLi.className = "todo";
+  todoLi.innerHTML = ` <input type="checkbox" id="${todoId}" />
+          <label class="custom-checkbox" for="${todoId}">
+            <svg
+              fill="transparent"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path
+                d="M268-240 42-466l57-56 170 170 56 56-57 56Zm226 0L268-466l56-57 170 170 368-368 56 57-424 424Zm0-226-57-56 198-198 57 56-198 198Z"
+              />
+            </svg>
+          </label>
+          <label for="${todoId}" class="todo-text">
+            ${todo}
+          </label>
+          <button class="delete-button">
+            <svg
+              fill="var(--secondary-color)"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+              />
+            </svg>
+          </button>`;
+  return todoLi;
 }
