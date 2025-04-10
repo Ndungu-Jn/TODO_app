ed389b57d177fbf5f3c950b69a8a2497073498a4
John Ndungu <ndungulink@gmail.com>
2025-04-10 09:40:15 +0300

    initialized my js file

---
+++ app.js
@@ -0,0 +1,23 @@
+// what features do I want to impliment
+//1. Add todo using the text field
+//2. Mark todo as completed
+//3. Delete todo
+//4.save the to dos to local storage
+//5. Load todos from local storage when the page loads
+
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+let allTodos = [];
+// Load todos from local storage when the page loads
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  addTodo();
+});
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  alert(todoText);
+}
